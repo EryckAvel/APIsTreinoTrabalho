438e9a89f53c1721631f3ae5fa8ba6cd6a9f5244
EryckAvel <eryckavel@gmail.com>
2022-11-21 09:47:37 -0300

    remoção de lixo na classe principal - API VENDAS

--- vendas/src/main/java/io/github/eryckavel/vendas/VendasApplication.java
+++ vendas/src/main/java/io/github/eryckavel/vendas/VendasApplication.java
@@ -1,12 +1,8 @@
 package io.github.eryckavel.vendas;
 
-import io.github.eryckavel.vendas.model.Cliente;
-import io.github.eryckavel.vendas.repository.ClientesRepositorys;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.CommandLineRunner;
+
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.context.annotation.Bean;
 
 @SpringBootApplication
 public class VendasApplication {
