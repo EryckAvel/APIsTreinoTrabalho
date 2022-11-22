0b441aebe90ebbd7c70c0fdfe42677c29f79f7d4
EryckAvel <eryckavel@gmail.com>
2022-11-22 16:43:26 -0300

    Primeiro Rest da API- API SPRING3

---
+++ api-gateway/src/main/java/br/com/erudio/apigateway/Greeting.java
@@ -0,0 +1,13 @@
+package br.com.erudio.apigateway;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+
+@Getter
+@AllArgsConstructor
+public class Greeting {
+
+    private long id;
+    private String content;
+
+}
