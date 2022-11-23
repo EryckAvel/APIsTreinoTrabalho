3d24fa0de75edd91bf1eb7dd79b50b150931449f
EryckAvel <eryckavel@gmail.com>
2022-11-23 12:46:35 -0300

    Refatoração das clases - API SPRING3

--- api-gateway/src/main/java/br/com/erudio/apigateway/controller/MathController.java
+++ api-gateway/src/main/java/br/com/erudio/apigateway/controller/MathController.java
@@ -1,11 +1,12 @@
 package br.com.erudio.apigateway.controller;
 
+import br.com.erudio.apigateway.convertes.NumberConverter;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.concurrent.atomic.AtomicLong;
 
 @RestController
-public class GreetingController {
+public class MathController {
 
     private final AtomicLong counter = new AtomicLong();
     @RequestMapping(value = "/soma/{numberOne}/{numberTwo}",
@@ -13,10 +14,10 @@ public class GreetingController {
     public double soma(
             @PathVariable(value = "numberOne") String numberOne,
             @PathVariable(value = "numberTwo") String numberTwo) throws Exception {
-        if (!isNumeric(numberOne) || !isNumeric(numberTwo)) {
+        if (!NumberConverter.isNumeric(numberOne) || !NumberConverter.isNumeric(numberTwo)) {
             throw new UnsupportedOperationException("Por favor insira um valor numerico!");
         }
-        return convertToDouble(numberOne) + convertToDouble(numberTwo);
+        return NumberConverter.convertToDouble(numberOne) + NumberConverter.convertToDouble(numberTwo);
     }
 
     @RequestMapping(value = "/multiplicacao/{numberOne}/{numberTwo}",
@@ -24,10 +25,10 @@ public class GreetingController {
     public double multiplicacao(
             @PathVariable(value = "numberOne") String numberOne,
             @PathVariable(value = "numberTwo") String numberTwo) throws Exception {
-        if (!isNumeric(numberOne) || !isNumeric(numberTwo)) {
+        if (!NumberConverter.isNumeric(numberOne) || !NumberConverter.isNumeric(numberTwo)) {
             throw new UnsupportedOperationException("Por favor insira um valor numerico!");
         }
-        return convertToDouble(numberOne) * convertToDouble(numberTwo);
+        return NumberConverter.convertToDouble(numberOne) * NumberConverter.convertToDouble(numberTwo);
     }
 
     @RequestMapping(value = "/divisao/{numberOne}/{numberTwo}",
@@ -35,10 +36,10 @@ public class GreetingController {
     public double divisao(
             @PathVariable(value = "numberOne") String numberOne,
             @PathVariable(value = "numberTwo") String numberTwo) throws Exception {
-        if (!isNumeric(numberOne) || !isNumeric(numberTwo)) {
+        if (!NumberConverter.isNumeric(numberOne) || !NumberConverter.isNumeric(numberTwo)) {
             throw new UnsupportedOperationException("Por favor insira um valor numerico!");
         }
-        return convertToDouble(numberOne) / convertToDouble(numberTwo);
+        return NumberConverter.convertToDouble(numberOne) / NumberConverter.convertToDouble(numberTwo);
     }
 
     @RequestMapping(value = "/subtracao/{numberOne}/{numberTwo}",
@@ -46,10 +47,10 @@ public class GreetingController {
     public double subtracao(
             @PathVariable(value = "numberOne") String numberOne,
             @PathVariable(value = "numberTwo") String numberTwo) throws Exception {
-        if (!isNumeric(numberOne) || !isNumeric(numberTwo)) {
+        if (!NumberConverter.isNumeric(numberOne) || !NumberConverter.isNumeric(numberTwo)) {
             throw new UnsupportedOperationException("Por favor insira um valor numerico!");
         }
-        return convertToDouble(numberOne) - convertToDouble(numberTwo);
+        return NumberConverter.convertToDouble(numberOne) - NumberConverter.convertToDouble(numberTwo);
     }
 
     @RequestMapping(value = "/media/{numberOne}/{numberTwo}",
@@ -57,10 +58,10 @@ public class GreetingController {
     public double media(
             @PathVariable(value = "numberOne") String numberOne,
             @PathVariable(value = "numberTwo") String numberTwo) throws Exception {
-        if (!isNumeric(numberOne) || !isNumeric(numberTwo)) {
+        if (!NumberConverter.isNumeric(numberOne) || !NumberConverter.isNumeric(numberTwo)) {
             throw new UnsupportedOperationException("Por favor insira um valor numerico!");
         }
-        return (convertToDouble(numberOne) + convertToDouble(numberTwo))/2;
+        return (NumberConverter.convertToDouble(numberOne) + NumberConverter.convertToDouble(numberTwo))/2;
     }
 
     @RequestMapping(value = "/raiz/{numberOne}/{numberTwo}",
@@ -68,23 +69,10 @@ public class GreetingController {
     public double raiz(
             @PathVariable(value = "numberOne") String numberOne,
             @PathVariable(value = "numberTwo") String numberTwo) throws Exception {
-        if (!isNumeric(numberOne) || !isNumeric(numberTwo)) {
+        if (!NumberConverter.isNumeric(numberOne) || !NumberConverter.isNumeric(numberTwo)) {
             throw new UnsupportedOperationException("Por favor insira um valor numerico!");
         }
-        return Math.sqrt(convertToDouble(numberOne)) + Math.sqrt(convertToDouble(numberTwo));
+        return Math.sqrt(NumberConverter.convertToDouble(numberOne)) + Math.sqrt(NumberConverter.convertToDouble(numberTwo));
     }
 
-    private Double convertToDouble(String strNumber) {
-        if (strNumber == null) return 0D;
-        //10,25 -> 10.25
-        String number = strNumber.replaceAll(",",".");
-        if(isNumeric(number)) return Double.parseDouble(number);
-        return 0D;
-    }
-
-    private boolean isNumeric(String strNumber) {
-        if (strNumber == null)return false;
-        String number = strNumber.replaceAll(",",".");
-        return number.matches("[-+]?[0-9]*\\.?[0-9]+");
-    }
 }
