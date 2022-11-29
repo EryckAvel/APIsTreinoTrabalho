50ed33fc27df5ee5858a45cfccefcd4116619701
EryckAvel <eryckavel@gmail.com>
2022-11-29 09:57:17 -0300

    Update authenticação com spring security JDBC- API estacionamento

--- estacionamento/src/main/java/com/api/estacionamento/EstacionamentoApplication.java
+++ estacionamento/src/main/java/com/api/estacionamento/EstacionamentoApplication.java
@@ -3,6 +3,7 @@ package com.api.estacionamento;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -12,6 +13,7 @@ public class EstacionamentoApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(EstacionamentoApplication.class, args);
+		System.out.println(new BCryptPasswordEncoder().encode("senha123"));
 	}
 
 	@GetMapping("/")
---
+++ estacionamento/src/main/java/com/api/estacionamento/repository/UserRepository.java
@@ -0,0 +1,11 @@
+package com.api.estacionamento.repository;
+
+import com.api.estacionamento.model.UserModel;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import java.util.UUID;
+
+@Repository
+public interface UserRepository extends JpaRepository<UserModel, UUID> {
+}
