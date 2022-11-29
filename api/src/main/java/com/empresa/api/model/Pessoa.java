4e78ab8e0df046ea870af77206aed70a0dd8816e
EryckAvel <eryckavel@gmail.com>
2022-11-29 17:30:03 -0300

    primeiro model da API - API negocios

---
+++ api/src/main/java/com/empresa/api/model/Pessoa.java
@@ -0,0 +1,75 @@
+package com.empresa.api.model;
+
+import org.springframework.web.bind.annotation.GetMapping;
+
+import javax.persistence.*;
+import java.util.Objects;
+
+@Entity
+@Table(name = "tb_pessoa")
+public class Pessoa {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    @Column(nullable = false, length = 100)
+    private String nome;
+    @Column(nullable = false, length = 100)
+    private String sobrenome;
+    @Column(nullable = false)
+    private int idade;
+    @Column(nullable = false, length = 11)
+    private String cpf;
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (!(o instanceof Pessoa pessoa)) return false;
+        return getIdade() == pessoa.getIdade() && Objects.equals(getId(), pessoa.getId()) && Objects.equals(getNome(), pessoa.getNome()) && Objects.equals(getSobrenome(), pessoa.getSobrenome()) && Objects.equals(getCpf(), pessoa.getCpf());
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(getId(), getNome(), getSobrenome(), getIdade(), getCpf());
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public String getSobrenome() {
+        return sobrenome;
+    }
+
+    public void setSobrenome(String sobrenome) {
+        this.sobrenome = sobrenome;
+    }
+
+    public int getIdade() {
+        return idade;
+    }
+
+    public void setIdade(int idade) {
+        this.idade = idade;
+    }
+
+    public String getCpf() {
+        return cpf;
+    }
+
+    public void setCpf(String cpf) {
+        this.cpf = cpf;
+    }
+}
