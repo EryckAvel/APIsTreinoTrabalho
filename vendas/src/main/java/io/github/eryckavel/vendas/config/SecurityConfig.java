da2d041a1861a04e5bd904220202f2e85f7223e8
EryckAvel <eryckavel@gmail.com>
2022-11-30 17:46:20 -0300

    Update de segurança na API - API VENDAS

--- vendas/src/main/java/io/github/eryckavel/vendas/config/SecurityConfig.java
+++ vendas/src/main/java/io/github/eryckavel/vendas/config/SecurityConfig.java
@@ -2,32 +2,40 @@ package io.github.eryckavel.vendas.config;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.http.HttpMethod;
-import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
-import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
-import org.springframework.security.web.SecurityFilterChain;
+
 
 @Configuration
-public class SecurityConfig {
-
-    @Bean
-    public SecurityFilterChain  filterChain(HttpSecurity http) throws Exception{
-        http
-                .httpBasic()
-                .and()
-                .authorizeHttpRequests()
-                .antMatchers(HttpMethod.GET, "/clientes"
-        return http.build();
-    }
+public class SecurityConfig extends WebSecurityConfigurerAdapter{
 
     @Bean
     public PasswordEncoder passwordEncoder(){
         return new BCryptPasswordEncoder();
     }
 
+    @Override
+    protected void configure(HttpSecurity http) throws Exception {
+        http
+                .csrf().disable()
+                .authorizeRequests()
+                    .antMatchers("/clientes/**")
+                        .authenticated()
+                    .antMatchers("/pedidos")
+                        .permitAll()
+                .and()
+                    .formLogin();
+    }
+
+    @Override
+    protected void configure(AuthenticationManagerBuilder auth) throws Exception {
+        auth
+                .inMemoryAuthentication()
+                .withUser("eryck")
+                .password(passwordEncoder().encode("3003"))
+                .roles("USER");
+    }
 }
