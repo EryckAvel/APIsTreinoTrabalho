f85ef0b312b90d2b9abe9887ef43667160a01671
EryckAvel <eryckavel@gmail.com>
2022-12-08 14:30:47 -0300

    Adicionando API RELACIONAL

---
+++ relacional/src/test/java/com/eryckavel/api/relacional/RelacionalApplicationTests.java
@@ -0,0 +1,13 @@
+package com.eryckavel.api.relacional;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RelacionalApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
