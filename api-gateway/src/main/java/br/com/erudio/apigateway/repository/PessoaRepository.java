536f51b93bb7374760db83c8fe843b3ff3ae5d0d
EryckAvel <eryckavel@gmail.com>
2022-12-08 14:25:48 -0300

    Limpando GIT

--- api-gateway/src/main/java/br/com/erudio/apigateway/repository/PessoaRepository.java
+++
@@ -1,8 +0,0 @@
-package br.com.erudio.apigateway.repository;
-
-import br.com.erudio.apigateway.model.Pessoa;
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface PessoaRepository extends JpaRepository<Pessoa, Long> { }
