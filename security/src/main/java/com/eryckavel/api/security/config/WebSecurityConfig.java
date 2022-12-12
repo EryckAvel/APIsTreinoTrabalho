4a0880cf275e2371838e3bdd3556e48f26c2cdcc
EryckAvel <eryckavel@gmail.com>
2022-12-12 12:11:18 -0300

    Concertando parte de Authenticação da API - API SECURITY

--- security/src/main/java/com/eryckavel/api/security/config/WebSecurityConfig.java
+++ security/src/main/java/com/eryckavel/api/security/config/WebSecurityConfig.java
@@ -2,14 +2,26 @@ package com.eryckavel.api.security.config;
 
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
-import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 
 @Configuration
 @EnableWebSecurity
-@EnableGlobalMethodSecurity(prePostEnabled = true)
 public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
+
+    @Override
+    protected void configure(HttpSecurity http) throws Exception {
+        http
+                .httpBasic()
+                .and()
+                .authorizeHttpRequests()
+                .antMatchers("/").permitAll()
+                .antMatchers("/hello").permitAll()
+                .antMatchers( "/admin").hasAnyRole("ADMIN")
+                .antMatchers("/user").hasAnyRole("USER", "ADMIN")
+                .anyRequest().authenticated();
+    }
 
     @Override
     protected void configure(AuthenticationManagerBuilder auth) throws Exception {
--- security/src/main/java/com/eryckavel/api/security/controller/OlaMundoController.java
+++ security/src/main/java/com/eryckavel/api/security/controller/OlaMundoController.java
@@ -6,22 +6,19 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
-@RequestMapping("/hello")
 public class OlaMundoController {
 
-    @GetMapping
+    @GetMapping("/hello")
     public String hello() {
         return "Ola, essa e uma String retornada via API!";
     }
 
     @GetMapping("/user")
-    @PreAuthorize("hasAnyRole('ADMIN', 'USER')")
     public String user(){
         return "Usuario autorizado!";
     }
 
     @GetMapping("/admin")
-    @PreAuthorize("hasAnyRole('ADMIN')")
     public String admin(){
         return "Administrador autorizado!";
     }
