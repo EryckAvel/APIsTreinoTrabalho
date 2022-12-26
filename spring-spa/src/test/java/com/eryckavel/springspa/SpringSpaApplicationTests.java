92abe81828acc4168f853944d6bda736cd9a276d
EryckAvel <eryckavel@gmail.com>
2022-12-26 10:33:24 -0300

    Adiconando nova API de estudos

---
+++ spring-spa/src/test/java/com/eryckavel/springspa/SpringSpaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.eryckavel.springspa;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringSpaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
